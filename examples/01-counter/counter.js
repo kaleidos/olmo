6803d7cf83e8ff514df10ef5abc00d77c96044e4
Anler Hernandez Peral <anler@anler.me>
2016-01-12 12:19:00 +0100

    Example: Simple counter

--- examples/01-counter/counter.js
+++ examples/01-counter/counter.js
@@ -1,15 +1,34 @@
 import { html } from 'olmo/jsx';
+import { onEvent } from 'olmo/html-events';
+import Type from 'olmo/actions';
 
 // model
 export function init(initialValue=0) {
   return initialValue;
 }
 
+// actions
+export const Action = Type({
+  Increment: [],
+  Decrement: []
+});
+
 // update
 export function update(action, model) {
-  return model;
+  return Action.case({
+    Increment: () => model + 1,
+    Decrement: () => model - 1
+  }, action);
 }
 
 export function view({address, model}) {
-  return <p>hola</p>;
+  return (
+    <div>
+      <p>{model}</p>
+      <p>
+        <button on-click={onEvent(address, Action.Decrement)}>Decrement</button>
+        <button on-click={onEvent(address, Action.Increment)}>Increment</button>
+      </p>
+    </div>
+  );
 }
