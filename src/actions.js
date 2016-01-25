8db5b74e55944ecf2df2ce51ce658c96ad748099
Anler Hernandez Peral <anler@anler.me>
2016-01-25 10:13:03 +0100

    New Action method: `Action.case`

--- src/actions.js
+++ src/actions.js
@@ -8,25 +8,27 @@ export function ActionType(spec) {
 }
 
 function buildActionConstructor([actionName, actionFields]) {
+  function actionGuard(name, handlers) {
+    return function update(action, model) {
+      let handler;
+      if (R.hasIn(action.type, handlers)) {
+        handler = handlers[action.type];
+      } else if (R.has(defaultHandlerName, handlers)) {
+        handler = handlers[defaultHandlerName];
+      } else {
+        throw new Error(`Unhandled action '${action.type}' in '${name}'`);
+      }
+      return handler(action, model);
+    };
+  }
   return {
     [actionName]: R.curryN(
       actionFields.length,
       (...args) => R.merge(R.zipObj(actionFields, args), {type: actionName})
     ),
 
-    getUpdateFor(name, handlers) {
-      return function update(action, model) {
-        let handler;
-        if (R.hasIn(action.type, handlers)) {
-          handler = handlers[action.type];
-        } else if (R.has(defaultHandlerName, handlers)) {
-          handler = handlers[defaultHandlerName];
-        } else {
-          throw new Error(`Unhandled action '${action.type}' in '${name}'`);
-        }
-        return handler(action, model);
-      };
-    }
+    getUpdateFor: actionGuard,
+    case: actionGuard
   };
 }
 
